48ffe5e68d136746cec7fc71a4eeaed3e636559f
Carlos Gonçalves <carlosdev65@gmail.com>
2022-07-18 19:40:33 -0300

    Added query in repository!

--- src/main/java/br/com/javadev/springboot/repository/ProdutoRepository.java
+++ src/main/java/br/com/javadev/springboot/repository/ProdutoRepository.java
@@ -2,6 +2,21 @@ package br.com.javadev.springboot.repository;
 
 import br.com.javadev.springboot.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    @Query("SELECT p FROM Produto p WHERE " +
+            "p.name LIKE CONCAT('%', :query, '%') " +
+            "Or p.descricao LIKE CONCAT('%', :query, '%')"
+    )
+    List<Produto> searchProdutos(String query);
+
+    @Query(value = "SELECT * FROM Produto p WHERE " +
+            "p.name LIKE CONCAT('%', :query, '%') " +
+            "Or p.descricao LIKE CONCAT('%', :query, '%')", nativeQuery = true
+    )
+    List<Produto> searchProdutosSQL(String query);
 }
